399f47be5ccbebeb0d113f97f3916746178afa43
Llanyro <Llanyro@gmail.com>
2022-07-07 17:51:20 +0200

    Adding simple print

--- example.cpp
+++ example.cpp
@@ -1,17 +1,4 @@
-#pragma region Extra
-#include <iostream>
-
-#define PRINT(a) std::cout << a
-#define PRINT2(a, b) std::cout << a << " " << b
-
-#define PRINTLN(a) std::cout << a << std::endl
-#define PRINT2LN(a, b) std::cout << a << " " << b << std::endl
-
-#define TAB putchar('\t')
-#define LN putchar('\n')
-#define ENDL LN;
-#pragma endregion
-
+#include "llanysimpleprint.hpp"
 #include "logger.hpp"
 
 
